639da861236a10934756efd681165798acd98dea
manmul-hyeonjang <3dncjf@gmail.com>
2026-09-07 12:19:57 +0900

    fix: keep photo work reports scoped and reliable

--- tests/version-sync.check.js
+++ tests/version-sync.check.js
@@ -18,7 +18,7 @@ const path = require('path');
 
 const ROOT = path.join(__dirname, '..');
 const fail = [];
-const TARGET_BUILD = 'hyeonjang-v269-mobilemore';
+const TARGET_BUILD = 'hyeonjang-v270-photoreport';
 
 const sw = fs.readFileSync(path.join(ROOT, 'sw.js'), 'utf8');
 const html = fs.readFileSync(path.join(ROOT, 'index.html'), 'utf8');
